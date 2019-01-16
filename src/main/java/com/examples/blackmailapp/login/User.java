1f6f58338fb867585f36580bf7a1148e20a4e8da
williammunnich <williammunnich@gmail.com>
2019-01-16 13:36:42 -0600

    added a few methods(not completed) within user

--- src/main/java/com/examples/blackmailapp/login/User.java
+++ src/main/java/com/examples/blackmailapp/login/User.java
@@ -1,5 +1,18 @@
 package com.examples.blackmailapp.login;
 
 public class User {
-    
+    int UserID;
+    String UserName;
+    String UserPassword;
+
+   //to call type this.getNextNewUser();
+   private int getNextNewUserInt(){
+       int nextNew = 5;
+       return nextNew;
+   }
+
+   /* contructor for user */
+   public int Create(String name, String password){
+        return 5;
+    }
 }
