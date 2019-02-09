c2b0dc9b4d229a4c938fbdd3fae358aa0d22fd5e
David <dhoc1901@mpsedu.org>
2019-02-08 21:14:21 -0600

    Revert "Calorie counter quality of life"

--- src/main/java/com/examples/blackmailapp/login/LoginResult.java
+++ src/main/java/com/examples/blackmailapp/login/LoginResult.java
@@ -10,9 +10,6 @@ import android.support.v4.content.res.ResourcesCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.backendless.Backendless;
 import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
@@ -22,10 +19,7 @@ public class LoginResult extends Activity {
 	static final String logoutButtonState_key = "LogoutButtonState";
 
 	private EditText backendlessUserInfo;
-	private TextView calorieSum;
 	private Button bkndlsLogoutButton;
-	private Button buttonCamera;
-	private Button buttonCalculate;
 
 	private String userInfo;
 
@@ -55,10 +49,7 @@ public class LoginResult extends Activity {
 
 	private void initUI() {
 		backendlessUserInfo = (EditText) findViewById(R.id.editText_bkndlsBackendlessUserInfo);
-		calorieSum = (TextView) findViewById(R.id.totalCaloriesAllTime);
 		bkndlsLogoutButton = (Button) findViewById(R.id.button_bkndlsBackendlessLogout);
-		buttonCamera = (Button) findViewById(R.id.buttonCamera);
-		buttonCalculate = (Button) findViewById(R.id.buttonCalculate);
 	}
 
 	private void initUIBehaviour() {
@@ -68,41 +59,8 @@ public class LoginResult extends Activity {
 					logoutFromBackendless();
 			}
 		});
-		buttonCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startCameraRun();
-            }
-        });
-		buttonCalculate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onImagePickerClicked();
-            }
-        });
-        Backendless.Persistence.of(Profile.class).findById(getIntent().getStringExtra("objectId"), new AsyncCallback<Profile>() {
-            @Override
-            public void handleResponse(Profile response) {
-                int totalCalories = response.getCaloric();
-                calorieSum.setText(totalCalories + " All-time Calories");
-            }
-
-            @Override
-            public void handleFault(BackendlessFault fault) {
-                Toast.makeText(LoginResult.this, "Update Failed", Toast.LENGTH_SHORT).show();
-            }
-        });
 	}
-    private void startCameraRun()
-    {
-        startActivity(new Intent(this, CameraRun.class));
-    }
-    private void onImagePickerClicked()
-    {
-        Intent intent = new Intent(this, ImagePickerActivity.class);
-        intent.putExtra("objectId", getIntent().getStringExtra("objectId"));
-    	startActivity(intent);
-    }
+
 	private void logoutFromBackendless(){
 		Backendless.UserService.logout(new AsyncCallback<Void>() {
 			@Override
--- src/main/java/com/examples/blackmailapp/login/RegisterActivity.java
+++ src/main/java/com/examples/blackmailapp/login/RegisterActivity.java
@@ -29,8 +29,6 @@ public class RegisterActivity extends Activity {
 	private String email;
 	private String password;
 
-    Profile profile = new Profile();
-
 	private BackendlessUser user;
 
 	public void onCreate(Bundle savedInstanceState) {
@@ -45,7 +43,6 @@ public class RegisterActivity extends Activity {
 		emailField = (EditText) findViewById(R.id.emailField);
 		passwordField = (EditText) findViewById(R.id.passwordField);
 		registerButton = (Button) findViewById(R.id.registerButton);
-
 
 		registerButton.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -77,35 +74,29 @@ public class RegisterActivity extends Activity {
 			name = nameText;
 		}
 
-        profile.setCaloric(0);
-        profile.setUserMail3(email);
-        Backendless.Persistence.save(profile, new AsyncCallback<Profile>() {
-            @Override
-            public void handleResponse(Profile response) {
-                BackendlessUser user = new BackendlessUser();
+		BackendlessUser user = new BackendlessUser();
 
-                if (email != null) {
-                    user.setEmail(email);
-                }
+		if (email != null) {
+			user.setEmail(email);
+		}
 
-                if (password != null) {
-                    user.setPassword(password);
-                }
+		if (password != null) {
+			user.setPassword(password);
+		}
 
-                if (name != null) {
-                    user.setProperty("name", name);
-                }
-                user.setProperty("ProfileId", profile.getObjectId());
-                Backendless.UserService.register(user, new AsyncCallback<BackendlessUser>() {
+		if (name != null) {
+			user.setProperty("name", name);
+		}
+
+
+		Backendless.UserService.register(user, new AsyncCallback<BackendlessUser>() {
 			@Override
 			public void handleResponse(BackendlessUser response) {
 				Resources resources = getResources();
-				String message = resources.getString(R.string.registration_success_message);
-				String message2 = resources.getString(R.string.app_name);
-				String message3 = message + " " + message2;
+				String message = String.format(resources.getString(R.string.registration_success_message), resources.getString(R.string.app_name));
 
 				AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-				builder.setMessage(message3).setTitle(R.string.registration_success);
+				builder.setMessage(message).setTitle(R.string.registration_success);
 				AlertDialog dialog = builder.create();
 				dialog.show();
 			}
@@ -118,12 +109,6 @@ public class RegisterActivity extends Activity {
 				dialog.show();
 			}
 		});
-            }
-
-            @Override
-            public void handleFault(BackendlessFault fault) {
-            }
-        });
 	}
 }
 
